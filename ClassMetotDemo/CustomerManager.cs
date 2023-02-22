b29711178924ee132d1c2d217dc93b954ed51401
enesments <enesments@gmail.com>
2023-02-22 12:47:40 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri Eklendi. " + customer.ID+" " + customer.FirstName+" " + customer.LastName+" " + customer.Gender);
+        }
+        public void Del(Customer customer)
+        {
+            Console.WriteLine("Müşteri Silindi. " + customer.ID + " " + customer.FirstName + " " + customer.LastName + " " + customer.Gender);
+        }
+        public void Listed(string FirstName, string LastName, string Gender, int ID)
+        {
+            Console.WriteLine("ID= " + ID);
+            Console.WriteLine("FirstName= " + FirstName);
+            Console.WriteLine("Lastname= " + LastName);
+            Console.WriteLine("Gender = " + Gender);
+        }
+        public void Update(params Customer[] customer)
+        {
+            foreach (var customers in customer)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("ID = " + customers.ID);
+                Console.WriteLine("FirstName = " + customers.FirstName);
+                Console.WriteLine("LastName = " + customers.LastName);
+                Console.WriteLine("Gender = " + customers.Gender);
+                Console.WriteLine("Customer is Updated....!");
+
+
+            }
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Gender = "Erkek";
+            customer1.FirstName = "Salih";
+            customer1.LastName = "Satın";
+            customer1.ID = 14;
+            Customer customer2 = new Customer();
+            customer2.Gender = "Kadın";
+            customer2.FirstName = "Ayse";
+            customer2.LastName = "Gül";
+            customer2.ID = 13;
+            Customer customer3 = new Customer();
+            customer3.Gender = "Kadın";
+            customer3.FirstName = "Aysel";
+            customer3.LastName = "Tepeci";
+            customer3.ID = 23;
+
+            Customer[] customer = new Customer[] { customer1, customer2, customer3 };
+
+            foreach (var customers in customer)
+            {
+                Console.WriteLine("ID= " + customers.ID);
+                Console.WriteLine("Name= " + customers.FirstName);
+                Console.WriteLine("Surname= " + customers.LastName);
+                Console.WriteLine("Gender= " + customers.Gender);
+            }
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2); 
+            customerManager.Add(customer3);
+
+            customerManager.Listed("Salih", "Satın", "Erkek", 14);
+            customerManager.Listed("Ayse", "Gül", "Kadın", 13);
+            customerManager.Listed("Aysel", "Tepeci", "Kadın", 23);
+            customerManager.Del(customer1);
+            customerManager.Del(customer2);
+            customerManager.Del(customer3);
+
+            customer1.ID = 25;
+            customer1.FirstName = "Ahmet";
+            customer1.LastName = "Baklavacı";
+            customer1.Gender = "Erkek";
+            customerManager.Update(customer1);
+
+
+        }
+    }
+}
